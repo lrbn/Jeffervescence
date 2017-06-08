fb3b66423cbb917898fdf82974361f499c5f36a9
brandon <lrbn86@gmail.com>
2017-06-08 16:02:53 -0400

    Update to main repo

--- README.md
+++ README.md
@@ -2,17 +2,36 @@
 
 ## Day 3 Homework
 
-* Store the flicks in an array, as well as in the DOM.
+* Store the flicks/dinosaurs in an array, as well as in the DOM.
 
 ### Mega Bonus Credit
 
-* Add a promote/fav button.
-* Add a delete button.
+* Add a promote/fav button, just like you did yesterday!
+* Add a delete button, just like yesterday!
 
 ### Super Mega Bonus Credit
 
-* Add buttons to move a flick up and down the list.
+* Add buttons to move a flick/dinosaur up and down the list.
 
 ### Super Mega Bonus Credit Hyper Fighting
 
-* Persist the flicks data using `window.localstorage`. The flicks should stay in the list even when the page is refreshed.
+* Persist the flicks/dinosaurs data using `window.localstorage`. The flicks/dinosaurs should stay in the list even when the page is refreshed.
+
+## Day 4 Homework
+
+* Fix the issues with flick id values, which sometimes results in removing the wrong flick from the array (and thus from localStorage).
+
+### Bonus Credit
+
+* Also track the year the flick was released.
+
+### Super Mega Bonus Credit
+
+* Edit the names of flicks that are already in the list (and make sure the changes persist across page loads). Gee, it would be nice if we could make that span's _content editable_ somehow.
+
+### Super Mega Bonus Credit Hyper Fighting
+
+* Have a good weekend.
+
+
+
--- app.js
+++ app.js
@@ -1,42 +1,96 @@
 const app = {
   init(selectors) {
-    this.flicks = [];
-    this.max = 0;
-    this.list = document.querySelector(selectors.listSelector);
-    this.flickform = document.querySelector(selectors.formSelector)
-    this.flickform.addEventListener("submit", this.addFlick.bind(this));
+    this.flicks = []
+    this.max = 0
+    this.list = document
+      .querySelector(selectors.listSelector)
+    this.template = document
+      .querySelector(selectors.templateSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addFlickViaForm.bind(this))
+
+    this.load()
   },
-  addFlick(ev) {
-    ev.preventDefault();
-    const f = ev.target;
+
+  load() {
+    // Get the JSON string out of localStorage
+    const flicksJSON = localStorage.getItem('flicks')
+
+    // Turn that into an array
+    const flicksArray = JSON.parse(flicksJSON)
+
+    // Set this.flicks to that array
+    if (flicksArray) {
+      flicksArray
+        .reverse()
+        .map(this.addFlick.bind(this))
+    }
+  },
+
+  addFlick(flick) {
+    const listItem = this.renderListItem(flick)
+    this.list
+      .insertBefore(listItem, this.list.firstChild)
+    
+    ++ this.max
+    this.flicks.unshift(flick)
+    this.save()
+  },
+
+  addFlickViaForm(ev) {
+    ev.preventDefault()
+    const f = ev.target
     const flick = {
       id: this.max + 1,
       name: f.flickName.value,
-    };
-    const li = document.createElement('li');
-    const item = this.buildItem(flick);
-    item.id="left";
-    const b = document.createElement('button');
-    b.className = 'success hollow button'
-    b.innerHTML = '+';
-    const s = document.createElement('span');
-    s.id="right";
-    s.appendChild(b);
-    li.appendChild(item);
-    li.appendChild(s);
-    this.list.insertBefore(li, this.list.firstChild);
-    this.flicks.push(item.textContent);
-    ++this.max;
-    this.flickform.reset();
+    }
+
+    this.addFlick(flick)
+
+    f.reset()
   },
-  buildItem(flick) {
-    const item = document.createElement("span");
-    item.textContent = flick.name;
-    return item;
+
+  save() {
+    localStorage
+      .setItem('flicks', JSON.stringify(this.flicks))
+
   },
-};
+
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flick-name')
+      .textContent = flick.name
+
+    item
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeFlick.bind(this))
+
+    return item
+  },
+
+  removeFlick(ev) {
+    const listItem = ev.target.closest('.flick')
+
+    // Find the flick in the array, and remove it
+    for (let i = 0; i < this.flicks.length; i++) {
+      const currentId = this.flicks[i].id.toString()
+      if (listItem.dataset.id === currentId) {
+        this.flicks.splice(i, 1)
+        break
+      }
+    }
+
+    listItem.remove()
+    this.save()
+  },
+}
 
 app.init({
-  formSelector: "#flick-form",
-  listSelector: "#flick-list",
-});
+  formSelector: '#flick-form',
+  listSelector: '#flick-list',
+  templateSelector: '.flick.template',
+})
